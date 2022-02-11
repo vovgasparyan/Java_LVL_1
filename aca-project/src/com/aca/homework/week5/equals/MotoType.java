d0f2c57cdc329ce1268a0aa7ce3fe8564aa35b63
Administrator <vovgasparyan@gmail.com>
2022-02-12 00:27:35 +0400

    The solution of the 'motorcycle problem'

---
+++ aca-project/src/com/aca/homework/week5/equals/MotoType.java
@@ -0,0 +1,6 @@
+package com.aca.homework.week5.equals;
+
+public enum MotoType {
+    SUZUKI,
+    YAMAHA
+}
---
+++ aca-project/src/com/aca/homework/week5/equals/Motorcycle.java
@@ -0,0 +1,24 @@
+package com.aca.homework.week5.equals;
+
+public class Motorcycle {
+
+    private MotoType motoType;
+    private Owner owner;
+
+    public Motorcycle(MotoType motoType, Owner owners) {
+        this.motoType = motoType;
+        this.owner = owners;
+    }
+
+    @Override
+    public String toString() {
+        return "The owner of mototype " + motoType + " is " + owner.getFirstName() + " " + owner.getLastName();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Motorcycle motorcycle = (Motorcycle) obj;
+
+        return motorcycle.motoType.equals(this.motoType);
+    }
+}

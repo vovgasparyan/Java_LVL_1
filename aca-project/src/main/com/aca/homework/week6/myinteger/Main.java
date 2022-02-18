ca5c24dc916d049dd0495a56dd4b05d5f38d38d6
Administrator <vovgasparyan@gmail.com>
2022-02-18 22:56:05 +0400

    The solution of the 'my integer cache'

---
+++ aca-project/src/main/com/aca/homework/week6/myinteger/Main.java
@@ -0,0 +1,16 @@
+package com.aca.homework.week6.myinteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyInteger myInteger1 = MyInteger.valueOf(-100);
+        MyInteger myInteger2 = MyInteger.valueOf(50);
+        MyInteger myInteger3 = MyInteger.valueOf(-100);
+
+
+        System.out.println(myInteger1);
+        System.out.println(myInteger2);
+        System.out.println(myInteger3);
+    }
+}
---
+++ aca-project/src/main/com/aca/homework/week6/myinteger/MyInteger.java
@@ -0,0 +1,31 @@
+package com.aca.homework.week6.myinteger;
+
+public class MyInteger {
+
+    private int value;
+    private static MyInteger[] integersArray = new MyInteger[201];
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public static MyInteger valueOf(int value) {
+        if (value < -101 || value > 100) {
+            System.out.println("Insert number between -100 to 100");
+            System.exit(0);
+        }
+        if (integersArray[value + 100] == null) {
+            create(value);
+        }
+        return integersArray[value + 100];
+    }
+
+    public static void create(int value) {
+        integersArray[value + 100] = new MyInteger(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Value is: " + value;
+    }
+}

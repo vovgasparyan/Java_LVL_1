24ffda75a5a9c0ed93782731dd6b5c7cda007e2d
Administrator <vovgasparyan@gmail.com>
2022-03-06 23:43:43 +0400

    The solution of the 'tuple2' problem Add tuple2.txt file In the tests methods change expect and actual values

--- aca-project/src/test/com/aca/homework/week9/tuple2/Tuple2Test.java
+++ aca-project/src/test/com/aca/homework/week9/tuple2/Tuple2Test.java
@@ -9,14 +9,14 @@ class Tuple2Test {
     @Test
     public void testStringInteger () {
         Tuple2<String, Integer> tuple2 = new Tuple2<>("text", 45);
-        Assertions.assertEquals("text", tuple2.getT1());
-        Assertions.assertEquals(45, tuple2.getT2());
+        Assertions.assertEquals(tuple2.getT1(), "text");
+        Assertions.assertEquals(tuple2.getT2(), 45);
     }
 
     @Test
     public void testLongInteger() {
         Tuple2<Long, Integer> tuple2 = new Tuple2<>(100L, 45);
-        Assertions.assertEquals(100, tuple2.getT1());
-        Assertions.assertEquals(45, tuple2.getT2());
+        Assertions.assertEquals(tuple2.getT1(), 100);
+        Assertions.assertEquals(tuple2.getT2(), 45);
     }
 }
